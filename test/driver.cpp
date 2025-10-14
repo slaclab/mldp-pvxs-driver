fca1640c0087b8ae2475f139e097c36ee7f224c1
craftablescience <me@craftablescience.info>
2025-10-13 17:15:03 -0700

    feat: add additional tests

--- test/driver.cpp
+++ test/driver.cpp
@@ -15,6 +15,10 @@ TEST(pvxs_mldp_driver, counter) {
 
 	PVXSDPIngestionDriver driver{"test_provider", channel, {
 		"test:counter",
+		"test:voltage",
+		"test:status",
+		"test:waveform",
+		"test:table",
 	}, {}};
 	ASSERT_TRUE(driver);
 	driver.run();
