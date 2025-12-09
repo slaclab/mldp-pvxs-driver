5350f0d8e4cd3280783eacf08832ec4f050e0f65
Claudio Bisegni <bisegni@slac.stanford.edu>
2025-12-09 13:00:21 -0800

    refactor(epics): unify pv value conversion

--- src/reader/impl/epics/EpicsMLDPConversion.cpp
+++ src/reader/impl/epics/EpicsMLDPConversion.cpp
@@ -4,17 +4,22 @@ using namespace mldp_pvxs_driver::reader::impl::epics;
 
 void EpicsMLDPConversion::convertPVToProtoValue(const pvxs::Value& pvValue, DataValue* protoValue)
 {
-    const auto typeArraySetter = [protoValue](const auto& pvArray, auto setter)
+    const auto typeValueSetter = [&pvValue, protoValue]<typename T>(void (DataValue::*setter)(T))
+    {
+        (*protoValue.*setter)(pvValue.as<std::remove_cvref_t<T>>());
+    };
+
+    const auto typeArraySetter = [&pvValue, protoValue]<typename T>(void (DataValue::*setter)(T))
     {
         auto* protoArray = protoValue->mutable_arrayvalue();
-        for (const auto& elementValue : pvArray)
+        for (const auto& i : pvValue.as<pvxs::shared_array<const std::remove_cvref_t<T>>>())
         {
             auto* element = protoArray->add_datavalues();
-            (*element.*setter)(elementValue);
+            (*element.*setter)(i);
         }
     };
 
-    const auto structSetter = [](const pvxs::Value& structValue, DataValue* protoStruct)
+    static constexpr auto structSetter = [](const pvxs::Value& structValue, DataValue* protoStruct)
     {
         auto* structure = protoStruct->mutable_structurevalue();
         for (const auto& member : structValue.ichildren())
@@ -25,54 +30,49 @@ void EpicsMLDPConversion::convertPVToProtoValue(const pvxs::Value& pvValue, Data
             }
             auto* field = structure->add_fields();
             field->set_name(structValue.nameOf(member));
-            EpicsMLDPConversion::convertPVToProtoValue(member, field->mutable_value());
+            convertPVToProtoValue(member, field->mutable_value());
         }
     };
 
     switch (pvValue.type().code)
     {
-    case pvxs::TypeCode::Bool: protoValue->set_booleanvalue(pvValue.as<bool>()); break;
-    case pvxs::TypeCode::BoolA: typeArraySetter(pvValue.as<pvxs::shared_array<const bool>>(), &DataValue::set_booleanvalue); break;
+    case pvxs::TypeCode::Bool: return typeValueSetter(&DataValue::set_booleanvalue);
+    case pvxs::TypeCode::BoolA: return typeArraySetter(&DataValue::set_booleanvalue);
     case pvxs::TypeCode::Int8:
     case pvxs::TypeCode::Int16:
-    case pvxs::TypeCode::Int32: protoValue->set_intvalue(pvValue.as<int32_t>()); break;
-    case pvxs::TypeCode::Int64: protoValue->set_longvalue(pvValue.as<int64_t>()); break;
+    case pvxs::TypeCode::Int32: return typeValueSetter(&DataValue::set_intvalue);
+    case pvxs::TypeCode::Int64: return typeValueSetter(&DataValue::set_longvalue);
     case pvxs::TypeCode::UInt8:
     case pvxs::TypeCode::UInt16:
-    case pvxs::TypeCode::UInt32: protoValue->set_uintvalue(pvValue.as<uint32_t>()); break;
-    case pvxs::TypeCode::UInt64: protoValue->set_ulongvalue(pvValue.as<uint64_t>()); break;
+    case pvxs::TypeCode::UInt32: return typeValueSetter(&DataValue::set_uintvalue);
+    case pvxs::TypeCode::UInt64: return typeValueSetter(&DataValue::set_ulongvalue);
     case pvxs::TypeCode::Int8A:
     case pvxs::TypeCode::Int16A:
-    case pvxs::TypeCode::Int32A: typeArraySetter(pvValue.as<pvxs::shared_array<const int32_t>>(), &DataValue::set_intvalue); break;
-    case pvxs::TypeCode::Int64A: typeArraySetter(pvValue.as<pvxs::shared_array<const int64_t>>(), &DataValue::set_longvalue); break;
+    case pvxs::TypeCode::Int32A: return typeArraySetter(&DataValue::set_intvalue);
+    case pvxs::TypeCode::Int64A: return typeArraySetter(&DataValue::set_longvalue);
     case pvxs::TypeCode::UInt8A:
     case pvxs::TypeCode::UInt16A:
-    case pvxs::TypeCode::UInt32A: typeArraySetter(pvValue.as<pvxs::shared_array<const uint32_t>>(), &DataValue::set_uintvalue); break;
-    case pvxs::TypeCode::UInt64A: typeArraySetter(pvValue.as<pvxs::shared_array<const uint64_t>>(), &DataValue::set_ulongvalue); break;
-    case pvxs::TypeCode::Float32: protoValue->set_floatvalue(pvValue.as<float>()); break;
-    case pvxs::TypeCode::Float64: protoValue->set_doublevalue(pvValue.as<double>()); break;
-    case pvxs::TypeCode::Float32A: typeArraySetter(pvValue.as<pvxs::shared_array<const float>>(), &DataValue::set_floatvalue); break;
-    case pvxs::TypeCode::Float64A: typeArraySetter(pvValue.as<pvxs::shared_array<const double>>(), &DataValue::set_doublevalue); break;
-    case pvxs::TypeCode::String: protoValue->set_stringvalue(pvValue.as<std::string>()); break;
-    case pvxs::TypeCode::StringA:
-        typeArraySetter(
-            pvValue.as<pvxs::shared_array<const std::string>>(),
-            static_cast<void (DataValue::*)(const std::string&)>(&DataValue::set_stringvalue));
-        break;
+    case pvxs::TypeCode::UInt32A: return typeArraySetter(&DataValue::set_uintvalue);
+    case pvxs::TypeCode::UInt64A: return typeArraySetter(&DataValue::set_ulongvalue);
+    case pvxs::TypeCode::Float32: return typeValueSetter(&DataValue::set_floatvalue);
+    case pvxs::TypeCode::Float64: return typeValueSetter(&DataValue::set_doublevalue);
+    case pvxs::TypeCode::Float32A: return typeArraySetter(&DataValue::set_floatvalue);
+    case pvxs::TypeCode::Float64A: return typeArraySetter(&DataValue::set_doublevalue);
+    case pvxs::TypeCode::String: return typeValueSetter(&DataValue::set_stringvalue<const std::string&>);
+    case pvxs::TypeCode::StringA: return typeArraySetter(&DataValue::set_stringvalue<const std::string&>);
     case pvxs::TypeCode::Struct:
     case pvxs::TypeCode::Union:
-    case pvxs::TypeCode::Any: structSetter(pvValue, protoValue); break;
+    case pvxs::TypeCode::Any: return structSetter(pvValue, protoValue);
     case pvxs::TypeCode::StructA:
     case pvxs::TypeCode::UnionA:
     case pvxs::TypeCode::AnyA:
         {
-            const auto pvArray = pvValue.as<pvxs::shared_array<const pvxs::Value>>();
-            for (const auto& structValue : pvArray)
+            for (const auto& i : pvValue.as<pvxs::shared_array<const pvxs::Value>>())
             {
-                structSetter(structValue, protoValue->mutable_arrayvalue()->add_datavalues());
+                structSetter(i, protoValue->mutable_arrayvalue()->add_datavalues());
             }
-            break;
+            return;
         }
-    case pvxs::TypeCode::Null: protoValue->set_stringvalue("null"); break;
+    case pvxs::TypeCode::Null: return protoValue->set_stringvalue("null");
     }
 }
